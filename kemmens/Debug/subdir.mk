d1c97ed4a99e339e562bf0471a6098a1aabce69d
Matias Mendez Corridoni <matim97@gmail.com>
2018-09-01 23:03:06 -0300

    Prueba de gitignore

---
+++ kemmens/Debug/subdir.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../CommandInterpreter.c \
+../SocketClient.c \
+../SocketCommons.c \
+../SocketServer.c \
+../ThreadManager.c \
+../config.c \
+../logger.c \
+../megekemmen.c 
+
+OBJS += \
+./CommandInterpreter.o \
+./SocketClient.o \
+./SocketCommons.o \
+./SocketServer.o \
+./ThreadManager.o \
+./config.o \
+./logger.o \
+./megekemmen.o 
+
+C_DEPS += \
+./CommandInterpreter.d \
+./SocketClient.d \
+./SocketCommons.d \
+./SocketServer.d \
+./ThreadManager.d \
+./config.d \
+./logger.d \
+./megekemmen.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -pthread -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
