139fc7b44c60efccab76539f4e54a2d5c80c8464
pepeciavirella <pep.ciavirella@gmail.com>
2018-09-03 01:08:28 -0300

    Documentado config.h y megekemmen.h

--- kemmens/kemmens/megekemmen.h
+++ kemmens/kemmens/megekemmen.h
@@ -9,7 +9,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void exit_gracefully(int);
-void exit_gracefully_custom(void (*operations)(),int);
+/**
+ * 		ACCION: Destruye el CommandInterpreter y el log, y finaliza el programa
+ * 		PARAMETROS:
+ * 			exit_code: Codigo de salida (0 en caso de exito)
+ */
+void exit_gracefully(int exit_code);
+
+/**
+ * 		ACCION: Ejecuta una cierta funcion, y luego destruye el CommandInterpreter y el log, y finaliza el programa
+ * 		PARAMETROS:
+ * 			operations(): Funcion (que no recibe parametros) a ejecutar previo a terminar
+ * 			exit_code: Codigo de salida (0 en caso de exito)
+ */
+void exit_gracefully_custom(void (*operations)(), int exit_code);
 
 #endif /* MEGEKEMMEN_H_ */
