19809450e9a05e4c8e603e21cb9dd3590450f965
pepeciavirella <pep.ciavirella@gmail.com>
2018-09-06 20:40:31 -0300

    Conexion DAM en curso; funcion para conectar a IP por parametro

--- kemmens/SocketClient.c
+++ kemmens/SocketClient.c
@@ -25,3 +25,38 @@ int SocketClient_ConnectToServer(char* port)
 
 	return ((retorno < 0) ? -1 : server_socket);
 }
+
+int SocketClient_ConnectToServerIP(char* ip, char* puerto)
+{
+
+	struct addrinfo auxiliar;		//Temporal, para guardar transitoriamente datos de la direccion a la cual me quiero conectar
+	struct addrinfo* infoServidor;	//La posta, ahi hare el getaddrinfo
+
+	memset(&auxiliar, 0, sizeof(auxiliar));
+	auxiliar.ai_family = AF_UNSPEC;    		//El sistema verifica que familia de direcciones usaremos (IPv4 o IPv6)
+	auxiliar.ai_socktype = SOCK_STREAM;  	//Protocolo a usar es TCP/IP
+
+	getaddrinfo(ip, puerto, &auxiliar, &infoServidor);	//Guardo info de la direccion aca
+
+	int socketServidor;						//Descriptor del socket con la direccion del server
+	//Preparamos el socket con el tipo de conexion deseado
+	socketServidor = socket(infoServidor->ai_family, infoServidor->ai_socktype, infoServidor->ai_protocol);
+
+	if(socketServidor == -1)				//Si al querer armarlo devolvio <0, algo salio mal
+		return -1;
+
+	// Ahora si, queremos conectarnos al servidor a traves de ese socket!
+	int retorno = connect(socketServidor, infoServidor->ai_addr, infoServidor->ai_addrlen);
+
+	freeaddrinfo(infoServidor);				//Es esto o memory leak
+
+	if(retorno < 0)
+	{
+		return -1;
+	}
+	else
+	{
+		return socketServidor;				//Todo marcha bien Milhouse!
+	}
+
+}
--- kemmens/kemmens/SocketClient.h
+++ kemmens/kemmens/SocketClient.h
@@ -15,4 +15,14 @@
  */
 int SocketClient_ConnectToServer(char* port);
 
+/**
+ * 		ACCION: Establece una conexion como cliente a un servidor con cierta IP, en cierto puerto
+ * 		PARAMETROS:
+ * 			ip: IP del servidor al cual quiero conectarme
+ * 			port: Puerto de la IP a la cual quiero conectarme, en forma de cadena
+ * 		RETORNO: El numero de fd del socket a traves del cual se conecto, o bien -1 en caso de error
+ * 		(Nombre puesto en ingles asi Mati no me bardea :o)
+ */
+int SocketClient_ConnectToServerIP(char* ip, char* puerto);
+
 #endif /* SOCKETCLIENT_H_ */
