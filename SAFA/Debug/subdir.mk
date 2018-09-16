0fda6f4ffe6475b176fc802a9068165e157f761e
Matias Mendez Corridoni <matim97@gmail.com>
2018-09-16 00:24:13 -0300

    modifico gitignore y config de eclipse para que pushee los directorios Debug

---
+++ SAFA/Debug/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../CPUsManager.c \
+../ConsoleHandler.c \
+../main.c 
+
+OBJS += \
+./CPUsManager.o \
+./ConsoleHandler.o \
+./main.o 
+
+C_DEPS += \
+./CPUsManager.d \
+./ConsoleHandler.d \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/utnso/Documentos/tp-2018-2c-nuGetChar/kemmens" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
