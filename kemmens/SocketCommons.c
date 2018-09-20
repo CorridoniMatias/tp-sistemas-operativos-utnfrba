32948b7520d9b983c5a67e0efefeeb31d11cac06
pepeciavirella <pep.ciavirella@gmail.com>
2018-09-19 23:20:05 -0300

    Avances en #12. Me estan haciendo la cama...

--- kemmens/SocketCommons.c
+++ kemmens/SocketCommons.c
@@ -118,19 +118,45 @@ int SocketCommons_SendData(int socket, int message_type, void* data, int dataLen
 	return status;
 }
 
-void cerrarSocket(int descriptorSocket){
-      if((close(descriptorSocket) !=0 ))
+void SocketCommons_DeserializeContent(char* serializedContent, void** dataArray)
+{
+
+	int bytesToRead;		//Offset de los bytes que voy a leer; es el entero recibido antes de cada campo
+	int base = 0;			//Indice de la cadena serializada en el cual estoy leyendo actualmente; arranca en 0
+	int fieldNumber = 0;	//Indice del array donde quiero copiar el campo actual; arranca en 0
+
+	bytesToRead = serializedContent[base++]; //Leo la longitud del proximo campo a leer; adelanto en uno el indice para leer lo siguiente
+	if(isdigit(serializedContent[base]))	 //Hago esto en el caso de que el tamanio del proximo campo fueran dos digitos
+	{
+		bytesToRead *= 10;					      //Multiplico por 10 el valor actual, le sumo el siguiente (en el que estoy
+		bytesToRead += serializedContent[base++]; //parado ahora) y adelanto de nuevo el puntero, asi no queda rezagado
+	}
+
+	while(bytesToRead != 0)				//Si 0 fuese la cantidad de bytes a leer, es porque ya no hay mas campos
+	{
+		//Copio la data al elemento correspondiente de datArray
+		memcpy(&(dataArray[fieldNumber++]), &(serializedContent[base]), bytesToRead);
+		base += bytesToRead;	//Adelanto el puntero de la cadena serializada el offset de lo que recien lei
+
+		bytesToRead = serializedContent[base++];	//De nuevo, leo el proximo tamanio y verifico la cantidad de digitos
+		if(isdigit(serializedContent[base]))
+		{
+			bytesToRead *= 10;
+			bytesToRead += serializedContent[base++];
+		}
+	}
+
+}
+
+void SocketCommons_CloseSocket(int socketFD)
+{
+      if((close(socketFD) !=0 ))
      {
-    	 Logger_Log(LOG_ERROR, "No se pudo cerrar el socket descripto por %d", descriptorSocket);
+    	 Logger_Log(LOG_ERROR, "No se pudo cerrar el socket descripto por %d", socketFD);
     	 return;
      }
-     Logger_Log(LOG_ERROR, "Socket descripto por %d cerrado, fin de la conexion a traves de el", descriptorSocket);
+     Logger_Log(LOG_ERROR, "Socket descripto por %d cerrado, fin de la conexion a traves de el", socketFD);
      return;
  }
-
-
-
-
-
 
 
--- kemmens/kemmens/SocketCommons.h
+++ kemmens/kemmens/SocketCommons.h
@@ -3,11 +3,14 @@
 
 #include "commons/string.h"
 #include <string.h>
+#include <ctype.h>
 #include "logger.h"
 #include "SocketMessageTypes.h"
 #include <stdlib.h> // Para malloc
 #include <sys/socket.h> // Para crear sockets, enviar, recibir, etc
 #include <netdb.h> // Para getaddrinfo
+
+//------ESTRUCTURAS EMPLEADAS------//
 
 /**
  * 		Estructura que representa el encabezado de un mensaje, para ser interpretada segun el protocolo
@@ -19,6 +22,74 @@ typedef struct {
 	uint32_t body_length;
 	uint32_t message_type;
 } __attribute__((packed)) ContentHeader;
+
+/**
+ *		Estructura que sirve como paquete para agrupar dos campos de datos, sus tamanios, y el tamanio total.
+ *		La idea de los paquetes es guardar los datos a enviar ahi, serializarlos y enviar la cadena serializada
+ *		CAMPOS:
+ *			f1_length: Longitud del primer campo, en bytes (sin contar el \0 final)
+ *			field1: Cadena de datos del primer campo
+ *			f2_length: Longitud del segundo campo, en bytes (sin contar el \0 final)
+ *			field2: Cadena de datos del segundo campo
+ *			totalSize: Longitud total del paquete, en bytes (suma los largos de los campos, y el tamanio de sus lengths)
+ */
+typedef struct {
+	uint32_t f1_length;
+	char* field1;
+	uint32_t f2_length;
+	char* field2;
+	uint32_t totalSize;
+} TwoFieldedPackage;
+
+/**
+ *		Estructura que sirve como paquete para agrupar tres campos de datos, sus tamanios, y el tamanio total.
+ *		La idea de los paquetes es guardar los datos a enviar ahi, serializarlos y enviar la cadena serializada
+ *		CAMPOS:
+ *			f1_length: Longitud del primer campo, en bytes (sin contar el \0 final)
+ *			field1: Cadena de datos del primer campo
+ *			f2_length: Longitud del segundo campo, en bytes (sin contar el \0 final)
+ *			field2: Cadena de datos del segundo campo
+ *			f3_length: Longitud del tercer campo, en bytes (sin contar el \0 final)
+ *			field3: Cadena de datos del tercer campo
+ *			totalSize: Longitud total del paquete, en bytes (suma los largos de los campos, y el tamanio de sus lengths)
+ */
+typedef struct {
+	uint32_t f1_length;
+	char* field1;
+	uint32_t f2_length;
+	char* field2;
+	uint32_t f3_length;
+	char* field3;
+	uint32_t totalSize;
+} ThreeFieldedPackage;
+
+/**
+ *		Estructura que sirve como paquete para agrupar cuatro campos de datos, sus tamanios, y el tamanio total.
+ *		La idea de los paquetes es guardar los datos a enviar ahi, serializarlos y enviar la cadena serializada
+ *		CAMPOS:
+ *			f1_length: Longitud del primer campo, en bytes (sin contar el \0 final)
+ *			field1: Cadena de datos del primer campo
+ *			f2_length: Longitud del segundo campo, en bytes (sin contar el \0 final)
+ *			field2: Cadena de datos del segundo campo
+ *			f3_length: Longitud del tercer campo, en bytes (sin contar el \0 final)
+ *			field3: Cadena de datos del tercer campo
+ *			f4_length: Longitud del cuarto campo, en bytes (sin contar el \0 final)
+ *			field4: Cadena de datos del cuarto campo
+ *			totalSize: Longitud total del paquete, en bytes (suma los largos de los campos, y el tamanio de sus lengths)
+ */
+typedef struct {
+	uint32_t f1_length;
+	char* field1;
+	uint32_t f2_length;
+	char* field2;
+	uint32_t f3_length;
+	char* field3;
+	uint32_t f4_length;
+	char* field4;
+	uint32_t totalSize;
+} FourFieldedPackage;
+
+//------FUNCIONES DEFINIDAS------//
 
 /* Envia un string al socket indicado. Esta funcion ya envia el header automaticamente. Si el send del header falla devuelve -2.
  *
@@ -64,10 +135,19 @@ void* SocketCommons_ReceiveData(int socket, int* message_type, int* error_status
 int SocketCommons_SendData(int socket, int message_type, void* data, int dataLength);
 
 /*
+ * 	ACCION: Deserializar los campos de la cadena serializada que recibi con el getData y guardarlos en un array
+ * 			NOTA: Esta funcion debe llamarse luego de recibir el ContentHeader
+ * 	PARAMETROS:
+ * 		serializedContent: Cadena serializada que quiero deserializar
+ * 		dataArray: Array donde ir guardando los campos que voy deserializando (sin su longitud)
+ */
+void SocketCommons_DeserializeContent(char* serializedContent, void** dataArray);
+
+/*
  * 	ACCION: Cierra un socket; loguea el exito o fracaso en la clausura
  * 	PARAMETROS:
  * 		descriptorSocket: Descriptor numerico o fd del socket a cerrar
  */
-void cerrarSocket(int descriptorSocket);
+void SocketCommons_CloseSocket(int descriptorSocket);
 
 #endif /* SOCKETCOMMONS_H_ */
