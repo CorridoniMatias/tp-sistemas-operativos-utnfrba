8b9936357eb69fdc75f04a41b080da92fa909623
utnso <liloiascott@gmail.com>
2018-11-16 05:28:40 -0300

    messagetypes con safa

--- kemmens/kemmens/SocketMessageTypes.h
+++ kemmens/kemmens/SocketMessageTypes.h
@@ -19,8 +19,15 @@
 
 //PROTOCOLO DAM (2)
 
-#define MESSAGETYPE_DAM_ABRIR	20 //Comando: abrir <pathFile>
-#define MESSAGETYPE_DAM_FLUSH	21 //Comando flush <pathFile>
+//#define MESSAGETYPE_DAM_ABRIR	20 //Comando: abrir <pathFile>
+//#define MESSAGETYPE_DAM_FLUSH	21 //Comando flush <pathFile>
+
+#define MESSAGETYPE_DAM_SAFA_DUMMY	220 //Finaliza carga del script del dummy
+#define MESSAGETYPE_DAM_SAFA_ABRIR	221 //Comando: abrir
+#define MESSAGETYPE_DAM_SAFA_CREAR	222 //Comando: crear
+#define MESSAGETYPE_DAM_SAFA_BORRAR	223 //Comando: borrar
+#define MESSAGETYPE_DAM_SAFA_FLUSH	224 //Comando: flush
+#define MESSAGETYPE_DAM_SAFA_ERR	225 //Error en la operacion con SAFA
 
 ///PROTOCOLO S-AFA (5XX)
 //MENSAJES PARA EL CPU
