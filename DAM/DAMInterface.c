725a1026bc9664055e4a8ee17871f439db43469e
utnso <liloiascott@gmail.com>
2018-11-17 21:10:07 -0300

    DAM abrir le manda a SAFA error de operacion

--- DAM/DAMInterface.c
+++ DAM/DAMInterface.c
@@ -5,6 +5,41 @@
 //para el comando abrir
 void DAM_Abrir(void* arriveData)
 {
+	DeserializedData* data = Serialization_Deserialize(arriveData);
+
+	//verificamos que nos esten enviando los dos campos necesarios
+	if(data->count < 2) {
+		//no tenemos los datos necesarios para seguir adelante
+		printf("HAY UN PROBLEMA CON LOS DATOS RECIBIDOS\n");
+		return;
+	}
+
+	uint32_t idDTB = (uint32_t)data->parts[0];
+	char* filePath = (char*)data->parts[1];
+
+	//abrimos conexiones con el MDJ y el FM9
+	int socketMDJ = SocketClient_ConnectToServerIP(settings->ipMDJ, settings->puertoMDJ);
+	int socketFM9 = SocketClient_ConnectToServerIP(settings->ipFM9, settings->puertoFM9);
+
+	//verificamos que el archivo exista
+	uint32_t fStatus = SocketCommons_SendStringAsContent(socketMDJ, filePath, MESSAGETYPE_MDJ_CHECKFILE);
+	if (fStatus == 0) {
+		printf("El archivo solicitado con ubicacion %s no existe!\n", filePath);
+
+		//le comunico del error al SAFA y le paso el id del DTB
+		int socketSAFA = SocketClient_ConnectToServerIP(settings->ipSAFA, settings->puertoSAFA);
+		SerializedPart idForSAFA = {.size = sizeof(uint32_t), .data = idDTB};
+		SerializedPart* serializedContent = Serialization_Serialize(1, idForSAFA);
+		printf("Enviando error de operacion al SAFA\n");
+		SocketCommons_SendData(socketSAFA, MESSAGETYPE_DAM_SAFA_ERR, (void*)serializedContent->data, serializedContent->size);
+		free(serializedContent->data);
+		free(serializedContent);
+		free(data);
+		Serialization_CleanupDeserializationStruct(data);
+		free(arriveData);
+		return;
+	}
+
 	free(arriveData);
 }
 
---
+++ DAM/DAMhandler.c
@@ -0,0 +1,21 @@
+#include "headers/DAMhandler.h"
+
+void DummyHandler(int idDTB, char* path) {
+
+}
+
+void AbrirHandler(uint32_t idDTB, char* path) {
+
+}
+
+void CrearHandler(uint32_t idDTB, char* path) {
+
+}
+
+void BorrarHandler(uint32_t idDTB, char* path) {
+
+}
+
+void FlushHandler(uint32_t idDTB) {
+
+}
