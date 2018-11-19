a1709b1506288f94d6b929c5478de9da0a3148e3
Matias Mendez Corridoni <matim97@gmail.com>
2018-11-19 02:22:14 -0300

    Closes #35, Borrar Archivo DAM listo y testeado, arreglado error de concepto que no creaba todos los bloques al comienzo del FS, ahora no se borran los bloques cuando se liberan, arreglados unos bugs en DAM

--- kemmens/kemmens/Utils.h
+++ kemmens/kemmens/Utils.h
@@ -4,6 +4,7 @@
 #include <openssl/md5.h>
 #include <stdlib.h>
 #include <string.h>
+#include "commons/collections/dictionary.h"
 
 #define declare_and_init(pointer_name, data_type, init_value) 								\
 		data_type* pointer_name = (data_type*)malloc(sizeof(data_type)); 					\
