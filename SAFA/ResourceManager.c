7f713c53561dc1f5a97b45ce8687e95944846fb0
pepeciavirella <pep.ciavirella@gmail.com>
2018-12-11 21:17:40 -0300

    Agregado movimiento de recursos post-EXIT

--- SAFA/ResourceManager.c
+++ SAFA/ResourceManager.c
@@ -24,8 +24,36 @@ void AddNewResource(char* name)
 
 }
 
-void SignalForResource(char* name)
+void SignalForResource(char* name, uint32_t requesterID)
 {
+
+	//Closure para encontrar el DTB que solicita el recurso entre los DTBs de EXEC
+	bool IsRequesterDTB(void* aDTB)
+	{
+		DTB* realDTB = (DTB*) aDTB;
+		if(realDTB->id == requesterID)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	//Closure para hallar el recurso involucrado en la lista de recursos tomados de un DTB
+	bool IsInvolvedResource(void* res)
+	{
+		char* realRes = (char*) res;
+		if(strcmp(realRes, name) == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
 	//Si el recurso ya existe, aumento en uno sus instancias disponibles; obtengo sus datos y actualizo
 	if(dictionary_has_key(resources, name))
@@ -43,23 +71,38 @@ void SignalForResource(char* name)
 		AddNewResource(name);
 	}
 
+	//Si fuera 0 su ID (no por el Dummy, es mas por valor default) no intento tocar sus recursos en su estructura propia
+	if(requesterID != 0)
+	{
+		pthread_mutex_lock(&mutexEXEC);
+		//No me fijo que sea Null, ya deberia estar aca
+		DTB* requester = (DTB*) list_find(EXECqueue, IsRequesterDTB);
+		char* rName = list_find(requester->resourcesKept, IsInvolvedResource);
+		//Solo si ese recurso esta entre los tomados por el DTB involucrado, lo remuevo de esa lista
+		if(rName)
+		{
+			list_remove_by_condition(requester->resourcesKept, IsInvolvedResource);
+		}
+		pthread_mutex_unlock(&mutexEXEC);
+	}
+
 	//Otro puntero, apunta a los datos del recurso al que se le hizo signal
 	ResourceStatus* involved = (ResourceStatus*) dictionary_get(resources, name);
 
 	//Si el recurso tenia DTBs esperando a que se libere, debo desbloquear el primero y darle la instancia
 	if(!queue_is_empty(involved->waiters))
 	{
-		uint32_t* requesterID = malloc(sizeof(uint32_t));
+		uint32_t* firstWaiter = malloc(sizeof(uint32_t));
 		//Bajo la cantidad de instancias disponibles, ya que recien ahora habria podido hacer el wait
 		involved->availables--;
 		//Me guardo el ID del primer DTB que lo estaba esperando
-		*requesterID = *((uint32_t*) queue_pop(involved->waiters));
+		*firstWaiter = *((uint32_t*) queue_pop(involved->waiters));
 		//Actualizo los datos del recurso en el diccionario, hago un put sobre la misma key
 //		dictionary_putMAESTRO(resources, name, involved, ResourceDestroyer);
 
 		//Creo la estructura a meter en la cola; no modifica el PC del DTB (va en -1) ni debe registrar cambios en los archivos abiertos
 		UnlockableInfo* toBeAwakened = (UnlockableInfo*) malloc(sizeof(UnlockableInfo*));
-		toBeAwakened->id = *requesterID;
+		toBeAwakened->id = *firstWaiter;
 		toBeAwakened->newProgramCounter = -1;
 		toBeAwakened->openedFilesUpdate = dictionary_create();
 		toBeAwakened->appendOFs = true;
@@ -77,6 +120,20 @@ void SignalForResource(char* name)
 
 bool WaitForResource(char* name, uint32_t requesterID)
 {
+
+	//Closure para encontrar el DTB que solicita el recurso entre los DTBs de EXEC
+	bool IsRequesterDTB(void* aDTB)
+	{
+		DTB* realDTB = (DTB*) aDTB;
+		if(realDTB->id == requesterID)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
 	//Si el recurso no existe, lo creo y le pongo una instancia (la cual luego asignare)
 	if(!dictionary_has_key(resources, name))
@@ -97,18 +154,19 @@ bool WaitForResource(char* name, uint32_t requesterID)
 		*newWaiter = requesterID;
 		queue_push(waited->waiters, newWaiter);
 		Logger_Log(LOG_DEBUG, "SAFA::RESOURCES->El DTB de id %d esta esperando el recurso %s", *newWaiter, name);
-		//Actualizo mis registros de los recursos
-//		dictionary_putMAESTRO(resources, name, waited, ResourceDestroyer);
 		//Respuesta negativa
 		return false;
-//		return true;
 	}
 	//Si no es asi, puedo asignarlo sin problemas!
 	else
 	{
 		//Respuesta positiva
+		pthread_mutex_lock(&mutexEXEC);
+		//No me fijo que sea Null, ya deberia estar aca
+		DTB* requester = (DTB*) list_find(EXECqueue, IsRequesterDTB);
+		list_add(requester->resourcesKept, name);
+		pthread_mutex_unlock(&mutexEXEC);
 		return true;
-//		return false;
 	}
 
 }
